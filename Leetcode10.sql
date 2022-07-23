1f1f2695cd6f9d74e7929adf46988d87da778c8e
Asif <96224247+Asif1310@users.noreply.github.com>
2022-07-23 22:46:31 +0530

    Improving the query performance

--- Leetcode10.sql
+++ Leetcode10.sql
@@ -1,12 +1,12 @@
 # Write your MySQL query statement below
 
-SELECT sub.department AS Department, e1.name AS Employee, e1.salary AS Salary
+SELECT d.name AS Department, e1.name AS Employee, e1.salary AS Salary
 FROM
-(SELECT e.departmentId, d.name AS department, max(e.salary) AS maximum
+(SELECT e.departmentId, max(e.salary) AS maximum
 FROM Employee e
-JOIN Department d
-ON e.departmentId = d.id
-GROUP BY e.departmentId, d.name)sub
+GROUP BY e.departmentId)sub
 JOIN Employee e1
 ON e1.departmentId = sub.departmentId
 AND e1.salary = sub.maximum
+JOIN Department d
+ON e1.departmentId = d.id 
